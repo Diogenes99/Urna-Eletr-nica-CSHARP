afa364ec42478da965d7ddf6e9f65fa11cc54cec
Diogenes99 <162608859+Diogenes99@users.noreply.github.com>
2024-11-13 20:19:43 -0300

    Add files via upload

---
+++ pratica/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //olá mundo:
+            //Console.WriteLine("Olá, Mundo!");
+
+            //data types:
+
+            //int y = 10;
+            //int x = 20;
+            //int z = 30;
+            //melhor método:
+            //int y = 10,
+            //    x = 20,
+            //    z = 30;
+
+            //int idade = 24;
+            //Console.WriteLine(idade);
+
+            //long numeroGrande = 9999999999L;
+            //Console.WriteLine(numeroGrande);
+            //Console.WriteLine(long.MaxValue);
+            //Console.WriteLine(long.MinValue);
+
+            //double negativo = -55.2D;
+            //Console.WriteLine(negativo);
+            //Console.WriteLine(double.MaxValue);
+            //Console.WriteLine(double.MinValue);
+
+            //float precisao = 5.000001F;
+            //Console.WriteLine(precisao);
+            //Console.WriteLine(float.MaxValue);
+            //Console.WriteLine(float.MinValue);
+
+            //decimal dinheiro = 14.99M;
+            //Console.WriteLine(dinheiro);
+            //Console.WriteLine(decimal.MaxValue);
+            //Console.WriteLine(decimal.MinValue);
+
+            //string nome = "teste";
+            //char letra = 'a';
+
+            //Console.Write("Seu nome é ");
+            //Console.Write(nome);
+            //Console.WriteLine(letra);
+
+            string txtIdade = "-24";
+            int idade = Convert.ToInt32();
+
+            Console.ReadLine();
+        }
+    }
+}
